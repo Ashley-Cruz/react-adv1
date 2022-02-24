17e070fe79965abb8c9684f1c2087588ad0b38f2
Ashley Cruz <ashleynayellicruz@gmail.com>
2022-02-23 23:06:43 -0600

    Control Props Implementado

---
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { ProductInCart, Product } from '../interfaces/interfaces';
+
+export const useShoppingCart = () => {
+
+    const [shoppingCart, setShoppingCart] = useState<{[key: string]: ProductInCart}>({});
+
+    const onProductCountChange = ({count, product}: {count: number, product: Product}) => {
+        // console.log(count);
+        // console.log(product);
+        // setShoppingCart({
+        //     ...shoppingCart,
+        //     [product.id]: {...product, count}
+        // });
+        setShoppingCart(oldShoppingCart => {
+
+            const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0}
+            if(Math.max(productInCart.count + count, 0) > 0){
+                productInCart.count += count;
+                return {
+                    ...oldShoppingCart,
+                    [product.id]: productInCart
+                }
+            }
+            //Borra el producto
+            const {[product.id]: toDelete, ...rest} = oldShoppingCart;
+            return rest;
+
+            // if(count === 0){
+            //     const {[product.id]: toDelete, ...rest} = oldShoppingCart;
+            //     return rest;
+            // }
+            // return {
+            //     ...oldShoppingCart,
+            //     [product.id]: {...product, count}
+            // }
+        })
+    }
+
+    return {
+        shoppingCart,
+        onProductCountChange
+    }
+}
